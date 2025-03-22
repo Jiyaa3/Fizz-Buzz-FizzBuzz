6e6a098879d234cb78a1f0d9254532d4cef470ea
Jiyaa3 <jiyaaa1825@gmail.com>
2025-03-22 18:18:42 +0530

    Add files via upload

---
+++ CODE/main.py
@@ -0,0 +1,32 @@
+day = input("Enter the day: ")
+if day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "monday", "tuesday", "wednesday", "thursday", "friday"]:
+    print("\nMTWTF::")
+    print("\n6:30 - Good morningggggggggg.........")
+    print("7:15 - Let's go to college.. ")
+    print("2:00 - Welcome back to home... Have lunch ")
+    print("3:30 - Take a nap till 5:30 ... or do pending things... ")
+    print("5:30 - Go to the park with a book and walk listening to music..")
+    print("6:30 - Daily coding and upload on github")
+    print("7:30 - DS question practice")
+    print("8:30 - Have Dinner")
+    print("9:30 - Play Badminton")
+    print("10:30 - Come, play ludo and sleep")
+elif day in ["Saturday", "Sunday", "saturday", "sunday"]:
+    print("Saturday & Sunday")
+    print("10:30 - Good morningggggggggg.........")
+    print("11:15 - Eat something.. ")
+    print("12:00 - Play TT or draw.. ")
+    print("12:30 - Watch series.. ")
+    print("3:30 - Coding or pending or revise or practice...")
+    print("4:30 - Watch TMKOC")
+    print("5:30 - Go to the park with a book and walk listening to music..")
+    print("7:00 - Practice DS")
+    print("8:30 - Have Dinner")
+    print("9:30 - Play Badminton")
+    print("11:00 - Come, play ludo")
+    print("11:30 - Practice")
+else:
+    print("Enter a valid day!")
+
+
+
